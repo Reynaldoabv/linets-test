854ac18878edfefc90a453bd9f270cdc9e523d55
Reynaldo <reynaldob.webdev@gmail.com>
2019-05-13 16:39:10 -0400

    Adding JQuery animation to the button and navbar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-  $(document).scroll(function() {
-    var height = $('.navbar').height();
-
-    if ($(this).scrollTop() > height) {
-      $('.navbar').css('background-color', 'black');
+  // back to top button //
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 40) {
+      $('#top-btn').fadeIn();
     } else {
-      $('.navbar').css('background-color', 'transparent');
+      $('#top-btn').fadeOut();
+    }
+  });
+
+  $('#top-btn').click(function() {
+    $('html , body').animate({ scrollTop: 0 }, 800);
+  });
+
+  // Navbar hiding //
+  var lastScrollTop = 0;
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+
+    if (scrollTop - lastScrollTop > 50) {
+      var navHeight = $('.navbar').css('height');
+
+      $('.navbar').animate({ top: '-' + navHeight }, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({ top: '0px' }, 150);
+      lastScrollTop = scrollTop;
     }
   });
 });
